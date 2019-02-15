8f60a9889ceecc860afd5fd4faa9b96ae9c43b41
ismay <ismay@ismaywolff.nl>
2019-02-15 14:46:48 +0000

    18d9cd3 v28.4.0

---
+++ src/config.js
@@ -0,0 +1,38 @@
+import defaultConfig from './defaultConfig';
+import { isType, isString } from './lib/check';
+
+const DEFAULT_API_VERSION = 25;
+
+export default class Config {
+    static create(...args) {
+        const configObjects = args
+            .filter(arg => arg);
+
+        if (!configObjects.every(configObject => isType(configObject, 'object'))) {
+            throw new Error('Expected Config parameter to have type object');
+        }
+
+        return Object.assign({}, defaultConfig, ...args);
+    }
+
+    static processConfigForD2(config, d2) {
+        const api = d2.Api.getApi();
+        d2.model.ModelDefinition.prototype.api = api; // eslint-disable-line no-param-reassign
+        d2.models = d2.model.ModelDefinitions.getModelDefinitions(); // eslint-disable-line no-param-reassign
+
+        if (isString(config.baseUrl)) {
+            api.setBaseUrl(config.baseUrl);
+        } else {
+            api.setBaseUrl(`/api/${DEFAULT_API_VERSION}`);
+        }
+
+        if (config.i18n && config.i18n.sources) {
+            Array.from(config.i18n.sources)
+                .forEach(source => d2.i18n.addSource(source));
+        }
+
+        if (config.i18n && config.i18n.strings) {
+            d2.i18n.addStrings(Array.from(config.i18n.strings));
+        }
+    }
+}

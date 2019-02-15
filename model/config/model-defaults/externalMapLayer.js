ae0ed89d6fd3ceb85eec77ee69b34661e3167a25
ismay <ismay@ismaywolff.nl>
2019-02-15 15:22:14 +0000

    613d12c v31.3.0

---
+++ model/config/model-defaults/externalMapLayer.js
@@ -0,0 +1,11 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = {
+    mapService: 'WMS',
+    imageFormat: 'PNG',
+    mapLayerPosition: 'BASEMAP'
+};
+//# sourceMappingURL=externalMapLayer.js.map
